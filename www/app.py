76082ca3ef939e6af1d3c141f958b3407779feec
xiuyi.mei <873604424@qq.com>
2017-08-30 07:12:38 -0700

    add app.py

---
+++ www/app.py
@@ -0,0 +1,29 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+_author_ = 'meixiuyi'
+
+'''
+async web application.
+'''
+
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Meixiuyi</h1>')
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = await loop.create_server(app.make_handler(),'127.0.0.1', 8000)
+    logging.info('server started at http://127.0.0.1:8000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
